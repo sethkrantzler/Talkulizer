5b0de29e348837bd64b7264f2a8421c2059c58e4
Seth Krantzler <sethkrantzler@gmail.com>
2020-12-26 13:47:26 -0800

    Adding new visualizers and fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import ReactDOM from 'react-dom';
 import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { Canvas, useFrame, useResource, useThree } from 'react-three-fiber';
-import { BufferGeometry, Color, LineBasicMaterial, Mesh, Points, Scene, Vector2} from 'three';
+import { BufferGeometry, CircleBufferGeometry, CircleGeometry, Color, EdgesGeometry, Geometry, LineBasicMaterial, Mesh, Points, Scene, Vector2, DoubleSide} from 'three';
 import './App.css';
-import { MenuItem, Select } from '@material-ui/core';
+import { Input, MenuItem, Select, TextField } from '@material-ui/core';
 
 interface FrequencyRange {
   start: number;
   end: number;
+  color?: string;
 }
 
 function HorizontalLine(props: any) {
@@ -124,11 +125,214 @@ function VerticalLine(props: any) {
   );
 }
 
+function Plane(props: any) {
+  const geoRef = useRef<any>(null);
+  const lineRef = useRef<any>(null);
+  const freqRanges: FrequencyRange[] = [
+    {start: 0, end: 2, color: '#CFFFB3'},
+    {start: 4, end:  10, color: '#337CA0'},
+    {start: 13, end:  22, color: '#46237A'},
+    {start: 40, end:  88, color: '#FFB400'},
+    {start: 100, end:  256, color: '#EE5622'},
+    {start: 500, end:  852, color: '#3A5311'}
+  ];
+
+  let bufferLength = 0;
+  let amplitudeArray = new Uint8Array(0);
+
+  // Initialize vertices
+  const linePoints: Vector2[] = [];
+  const lineSegments = 500.0;
+  const size = 3.0;
+
+  for (let i = 0; i < lineSegments; i++) {
+    linePoints.push(new Vector2(0, size + (-2*size*i/lineSegments)));
+  }
+
+  useEffect(()=> {
+    if (!!props.analyzer && bufferLength == 0) {
+      bufferLength = props.analyzer.frequencyBinCount;
+      amplitudeArray = new Uint8Array(bufferLength);
+      props.analyzer.getByteFrequencyData(amplitudeArray);
+    }
+  });
+
+  function average(nums: Uint8Array) {
+    return nums.reduce((a, b) => (a + b)) / nums.length;
+  }
+
+  function getColor(){
+    let loudestAmp = 0;
+    let loudestBand = 0;
+    freqRanges.map((range, index) => {
+      const averageAmp = average(amplitudeArray.subarray(range.start, range.end));
+      if (averageAmp > loudestAmp) {
+        loudestBand = index;
+        loudestAmp = averageAmp;
+      }
+    });
+    return freqRanges[loudestBand].color;
+  }
+
+
+  useFrame(() => {
+    if (lineRef && lineRef.current && !!props.analyzer && amplitudeArray) {
+      props.analyzer.getByteFrequencyData(amplitudeArray);
+      lineRef.current.material.color.set(getColor());
+      lineRef.current.rotation.set(lineRef.current.rotation.x + 0.005, lineRef.current.rotation.y + 0.005, lineRef.current.rotation.z)
+    }
+  });
+  
+
+  return (
+    <mesh
+      ref={lineRef}
+      {...props}
+      scale={[1, 1, 1]}>
+      <planeBufferGeometry ref={geoRef} args={[3,3]} attach="geometry" />
+      <meshBasicMaterial color={'purple'} side={DoubleSide} attach="material"/>
+    </mesh>
+  );
+}
+
+function Ring(props: any) {
+  const geoRef = useRef<any>(null);
+  const lineRef = useRef<any>(null);
+
+  let bufferLength = 0;
+  let amplitudeArray = new Uint8Array(0);
+
+  // Initialize vertices
+  const linePoints: Vector2[] = [];
+  const lineSegments = 500.0;
+  const size = 5.0;
+
+  for (let i = 0; i < lineSegments; i++) {
+    linePoints.push(new Vector2(size + (-2*size*i/lineSegments), Math.random()));
+  }
+
+  useEffect(()=> {
+    if (!!props.analyzer && bufferLength == 0) {
+      bufferLength = props.analyzer.frequencyBinCount;
+      amplitudeArray = new Uint8Array(bufferLength);
+      props.analyzer.getByteFrequencyData(amplitudeArray);
+    }
+  });
+
+  function average(nums: Uint8Array) {
+    return nums.reduce((a, b) => (a + b)) / nums.length;
+  }
+
+  function graphFrequencyData(points: Vector2[], freqData: Uint8Array, freqRange: FrequencyRange, waveFunc?: string ) {
+    const freqArray = freqData.subarray(freqRange.start, freqRange.end);
+    const freqAvg = freqArray.length > 0 ? average(freqArray) : 0;
+    for (let i = 0; i < points.length; i++) {
+      points[i].y = (Math.pow(2, freqAvg/255.0) - 1)*Math.exp(-Math.abs(points[i].x)*0.65)*Math.cos(2*Math.PI*points[i].x + Date.now() / 400);
+    }
+    return points;
+  }
+
+
+  useFrame(() => {
+    if (geoRef && geoRef.current && !!props.analyzer && amplitudeArray && props.bolt) {
+      props.analyzer.getByteFrequencyData(amplitudeArray);
+      geoRef.current.setFromPoints(graphFrequencyData(linePoints, amplitudeArray, props.freqRange));
+    }
+    else if (lineRef && lineRef.current) {
+      lineRef.current.scale.set(lineRef.current.scale.x + props.scaleRate, lineRef.current.scale.y + props.scaleRate, lineRef.current.scale.z);
+      if (lineRef.current.scale.x > 10) {
+        lineRef.current.scale.set(0.01, 0.01, 1);
+        lineRef.current.position.set(lineRef.current.position.x, lineRef.current.position.y, lineRef.current.position.z + 0.0001)
+      }
+    }
+  });
+  
+
+  return (
+    <mesh
+      ref={lineRef}
+      {...props}
+      scale={[props.radius, props.radius, 1]}>
+      <ringBufferGeometry ref={geoRef} args={[1-(props.ringSize/2.0), 1+(props.ringSize/2.0), 1024]} attach="geometry" />
+      <meshBasicMaterial color={props.color} />
+    </mesh>
+  );
+}
+
+function Circle(props: any) {
+  const geoRef = useRef<any>(null);
+  const lineRef = useRef<any>(null);
+
+  let bufferLength = 0;
+  let amplitudeArray = new Uint8Array(0);
+
+  // Initialize vertices
+  const linePoints: Vector2[] = [];
+  const lineSegments = 500.0;
+  const size = 5.0;
+
+  for (let i = 0; i < lineSegments; i++) {
+    linePoints.push(new Vector2(size + (-2*size*i/lineSegments), Math.random()));
+  }
+
+  useEffect(()=> {
+    if (!!props.analyzer && bufferLength == 0) {
+      bufferLength = props.analyzer.frequencyBinCount;
+      amplitudeArray = new Uint8Array(bufferLength);
+      props.analyzer.getByteFrequencyData(amplitudeArray);
+    }
+  });
+
+  function average(nums: Uint8Array) {
+    return nums.reduce((a, b) => (a + b)) / nums.length;
+  }
+
+  function graphFrequencyData(points: Vector2[], freqData: Uint8Array, freqRange: FrequencyRange, waveFunc?: string ) {
+    const freqArray = freqData.subarray(freqRange.start, freqRange.end);
+    const freqAvg = freqArray.length > 0 ? average(freqArray) : 0;
+    for (let i = 0; i < points.length; i++) {
+      points[i].y = (Math.pow(2, freqAvg/255.0) - 1)*Math.exp(-Math.abs(points[i].x)*0.65)*Math.cos(2*Math.PI*points[i].x + Date.now() / 400);
+    }
+    return points;
+  }
+
+
+  useFrame(() => {
+    if (geoRef && geoRef.current && !!props.analyzer && amplitudeArray && props.bolt) {
+      props.analyzer.getByteFrequencyData(amplitudeArray);
+      geoRef.current.setFromPoints(graphFrequencyData(linePoints, amplitudeArray, props.freqRange));
+    }
+    else if (lineRef && lineRef.current) {
+      lineRef.current.scale.set(lineRef.current.scale.x + props.scaleRate, lineRef.current.scale.y + props.scaleRate, lineRef.current.scale.z);
+      if (lineRef.current.scale.x > 10) {
+        lineRef.current.scale.set(0.01, 0.01, 1);
+        lineRef.current.position.set(lineRef.current.position.x, lineRef.current.position.y, lineRef.current.position.z + 0.0001)
+      }
+    }
+  });
+  
+
+  return (
+    <mesh
+      ref={lineRef}
+      {...props}
+      scale={[props.bolt ? 1 : props.radius, props.bolt ? 1 : props.radius, 1]}>
+      <circleBufferGeometry ref={geoRef} args={[1, 1024]} attach="geometry" />
+      <meshBasicMaterial color={props.color} />
+    </mesh>
+  );
+}
+
 export default class App extends React.Component<any, any> {
 
   constructor(props: any) {
     super(props);
-    this.state = {analyzer: null, visualizerType: "horizontalLines"};
+    this.state = {
+      analyzer: null, 
+      visualizerType: "circular",
+      spread: 1,
+      offset: 1.3
+    };
   }
 
   componentDidMount(){
@@ -158,9 +362,7 @@ export default class App extends React.Component<any, any> {
     alert("Something went wrong: " + err.name);
   }
 
-  horizontalLines() {
-    const offset = 0;
-    const spread = 1.3;
+  horizontalLines(offset: number, spread: number) {
     return (
       <>
         <HorizontalLine analyzer={this.state.analyzer} position={[0, (spread*-2) - offset, -1]} color={'#46237A'} freqRange={{start: 0, end:  2}}/>
@@ -173,9 +375,49 @@ export default class App extends React.Component<any, any> {
     )
   }
 
-  verticalLines() {
-    const offset = 1;
-    const spread = 1.3;
+  solidColor() {
+    return (
+      <>
+        <Plane analyzer={this.state.analyzer} position={[0,0,0]} />
+      </>
+    )
+  }
+
+  circular(bolt: boolean) {
+    const numCircles = 6;
+    const maxRadius = 10;
+    const radiusScale=maxRadius/numCircles;
+    const scaleRate=0.01;
+    return (
+      <>
+        <Circle analyzer={this.state.analyzer} scaleRate={scaleRate} radius={5*radiusScale+scaleRate} color={'#46237A'} freqRange={{start: 0, end:  2}} bolt={bolt}/>
+        <Circle analyzer={this.state.analyzer} scaleRate={scaleRate} radius={4*radiusScale+scaleRate} color={'#FFB400'} freqRange={{start: 4,  end:  10}} bolt={bolt}/>
+        <Circle analyzer={this.state.analyzer} scaleRate={scaleRate} radius={3*radiusScale+scaleRate} color={'#CFFFB3'} freqRange={{start: 13, end:  22}} bolt={bolt}/>
+        <Circle analyzer={this.state.analyzer} scaleRate={scaleRate} radius={2*radiusScale+scaleRate} color={'#337CA0'} freqRange={{start: 40, end:  88}} bolt={bolt}/>
+        <Circle analyzer={this.state.analyzer} scaleRate={scaleRate} radius={1*radiusScale+scaleRate} color={'#EE5622'} freqRange={{start: 100, end:  256}} bolt={bolt}/>
+        <Circle analyzer={this.state.analyzer} scaleRate={scaleRate} radius={0*radiusScale+scaleRate} color={'#3A5311'} freqRange={{start: 500, end:  852}} bolt={bolt}/>
+      </>
+    )
+  }
+
+  rings(ringSize: number) {
+    const numRings = 6;
+    const maxRadius = 10;
+    const radiusScale=maxRadius/numRings;
+    const scaleRate=0.01;
+    return (
+      <>
+        <Ring analyzer={this.state.analyzer} scaleRate={scaleRate} radius={5*radiusScale+scaleRate} ringSize={ringSize} color={'#46237A'} freqRange={{start: 0, end:  2}} />
+        <Ring analyzer={this.state.analyzer} scaleRate={scaleRate} radius={4*radiusScale+scaleRate} ringSize={ringSize} color={'#FFB400'} freqRange={{start: 4,  end:  10}} />
+        <Ring analyzer={this.state.analyzer} scaleRate={scaleRate} radius={3*radiusScale+scaleRate} ringSize={ringSize} color={'#CFFFB3'} freqRange={{start: 13, end:  22}} />
+        <Ring analyzer={this.state.analyzer} scaleRate={scaleRate} radius={2*radiusScale+scaleRate} ringSize={ringSize} color={'#337CA0'} freqRange={{start: 40, end:  88}} />
+        <Ring analyzer={this.state.analyzer} scaleRate={scaleRate} radius={1*radiusScale+scaleRate} ringSize={ringSize} color={'#EE5622'} freqRange={{start: 100, end:  256}} />
+        <Ring analyzer={this.state.analyzer} scaleRate={scaleRate} radius={0*radiusScale+scaleRate} ringSize={ringSize} color={'#3A5311'} freqRange={{start: 500, end:  852}} />
+      </>
+    )
+  }
+
+  verticalLines(offset: number, spread: number) {
     return (
       <>
         <VerticalLine analyzer={this.state.analyzer} position={[(spread*-2) - offset, 0, -1]} color={'#46237A'} freqRange={{start: 0, end:  2}}/>
@@ -188,19 +430,28 @@ export default class App extends React.Component<any, any> {
     )
   }
 
-  renderVisualizer(visualizerType: string){
+  renderVisualizer(visualizerType: string, spread: number, offset: number){
     switch(visualizerType) { 
       case "horizontalLines": { 
-        return this.horizontalLines();
-        break; 
+        return this.horizontalLines(spread, offset);
       }
       case "verticalLines": { 
-        return this.verticalLines();
-        break; 
+        return this.verticalLines(spread, offset);
+      }
+      case "circular": { 
+        return this.circular(false);
+      }
+      case "bolt": { 
+        return this.circular(true);
+      } 
+      case "rings": { 
+        return this.rings(0.02);
+      } 
+      case "solid": { 
+        return this.solidColor();
       } 
       default: {
-        return this.horizontalLines();
-        break; 
+        return this.circular(true);
       } 
    } 
   }
@@ -209,11 +460,23 @@ export default class App extends React.Component<any, any> {
     this.setState({visualizerType: e.target.value});
   }
 
+  spreadChanged = (e: any) => {
+    this.setState({spread: e.target.value});
+  }
+
+  offsetChanged = (e: any) => {
+    this.setState({offset: e.target.value});
+  }
+
   render() {
 
     const options = [
       { value: 'horizontalLines', label: 'Horizontal Lines' },
-      { value: 'verticalLines', label: 'Vertical Lines' }
+      { value: 'verticalLines', label: 'Vertical Lines' },
+      { value: 'circular', label: 'Circles' },
+      { value: 'bolt', label: 'Lightning' },
+      { value: 'rings', label: 'Rings' },
+      { value: 'solid', label: 'Solid' }
     ];
 
     return (
@@ -223,11 +486,19 @@ export default class App extends React.Component<any, any> {
             onChange={this.visualizerChanged}>
               {options.map((o) => <MenuItem value={o.value}>{o.label}</MenuItem>)}
         </Select>
+        <TextField id="spread"
+            value={this.state.spread}
+            onChange={this.spreadChanged} 
+        />
+        <TextField id="offset"
+            value={this.state.offset}
+            onChange={this.offsetChanged} 
+        />
         <Canvas className={'App'}>
           <ambientLight intensity={0.5} />
           <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
           <pointLight position={[-10, -10, -10]} />
-          {this.renderVisualizer(this.state.visualizerType)}
+          {this.renderVisualizer(this.state.visualizerType, this.state.spread, this.state.offset)}
         </Canvas>
       </>
       
